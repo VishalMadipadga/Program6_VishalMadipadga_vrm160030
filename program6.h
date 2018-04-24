bbce3ec5e5a7dd3d6896ff5e99892e9be37ecdf7
VishalMadipadga <nbcuber@gmail.com>
2018-04-23 19:29:14 -0500

    adding header file

---
+++ program6.h
@@ -0,0 +1,40 @@
+#ifndef _PROGRAM6_H
+#define _PROGRAM6_H
+#include <stdint.h>
+#include <string>
+#include <sstream>
+class BinaryFileHeader
+{
+public:
+
+ uint32_t magicNumber;
+ uint32_t versionNumber;
+ uint64_t numRecords;
+
+
+
+};
+
+const int maxRecordStringLength = 25;
+
+class BinaryFileRecord
+{
+ public:
+
+ uint8_t strLength;
+ char stringBuffer[maxRecordStringLength];
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+#endif
